4898a7eb9ed94f6972a7eae56e169ecd70edbedc
MadgulaVamshi06 <vamshimadgula@gmail.com>
2024-08-02 13:14:31 +0530

    updated login

--- client/src/pages/Auth/Login.jsx
+++ client/src/pages/Auth/Login.jsx
@@ -17,7 +17,7 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post(`/api/api/login`,{
+      const res = await axios.post(`/api/login`,{
         email,
         password,
       }); 
